300d878ca5fff0fe550102d1ce8724188b6b5075
Zachary Solomon <trunkz77@live.com>
2023-05-24 21:16:15 -0400

    add sgb special and fix needle strike and bound interactions

--- README.md
+++ README.md
@@ -13,5 +13,19 @@ select parents
 replace population with children (inclusive?)
 
 issues:
-add cooldown logic when selecting abilities
-fix tracking Best rotations
+add logic to guarantee a valid ability is selected within n random tries
+implement preserving relative order to promote comboing needle strike with strong abilities
+
+ideas on training mutation algorithm to promote good rotations
+if parents ability in same order in rotation precedes abilities with high inflated base damage add a stronger weight to this ability
+track combos of any size
+small rotations of 2-20 abilities in order which gravitate together with a higher weight the higher the inflated dps is
+
+goal of a good rotation is to take each ability and use them in an order which maximizes the special effects of each ability to inflate base damage as high as possible
+
+ensure long duration buffs benefit high damaging abilities with percentage damage buffs (early deaths swiftness)
+increases flat damage of multihitting abilities (ecb before sgb bleeds grico)
+
+weigh ratio of proximity to damage increase relationship of each ability and store them in a matrix that updates slightly with each generation based on top 5 parents?
+
+drive 9
--- algorithm.py
+++ algorithm.py
@@ -3,20 +3,22 @@ fightDuration = 23 # 21.33 GCDs 38s
 generations=200
 numParents = 5
 numPopulation = 30
-numAbilities = 11
-numAbilityAttributes = 7 # 0 name, 1 skilltype, 2 adren, 3 damage, 4 cooldown, 5 duration, 6 hits
-abilities = [
+numAbilities = 12
+noneAbilityInd = numAbilities
 
-	['Piercing Shot', 'basic', 9, 90, 3, 1.8, 2],
+abilities = [ # 0 name, 1 skilltype, 2 adren, 3 damage, 4 cooldown, 5 duration, 6 hits
+    ['Piercing Shot', 'basic', 9, 90, 3, 1.8, 2],
 	['Binding Shot', 'basic', 9, 60, 15, 1.8, 1],
+    ['Fragmentation Shot', 'basic', 9, 120.6, 15, 1.8, 5],
+    ['Corruption Shot', 'basic', 9, 200, 15, 1.8, 5],
 	['Needle Strike', 'basic', 9, 94.2, 5.4, 1.8, 1],
-	['Fragmentation Shot', 'basic', 9, 120.6, 15, 1.8, 5],
 	['Snipe', 'basic', 9, 172, 10.2, 2.4, 1],
 	['Greater Ricochet', 'basic', 9, 160, 10.2, 1.8, 7],
 	['Rapid Fire', 'threshold', -15, 451.2, 20.4, 4.8, 8],
 	['Snap Shot', 'threshold', -15, 265, 20.4, 1.8, 2],
 	['Bombardment', 'threshold', -15, 131.4, 30, 1.8, 1],
 	['Tight Bindings', 'threshold', -15, 120, 15, 1.8, 1],
+    ["Crystal Rain", 'special', -27, 433.25, 30, 1.8, 2.44],
 	["Death's Swiftness", 'ultimate', -65, 315, 120, 1.8, 1],
     ["None", 'ultimate', 0, 0, 0, 0, 0]
 ]
@@ -25,52 +27,42 @@ population = [[0 for _ in range(fightDuration)] for _ in range(numPopulation)] #
 parents = [[0 for _ in range(fightDuration)] for _ in range(numParents)]
 parents[0] = [7272.642000000001, 10, 3, 4, 2, 6, 2, 4, 8, 0, 5, 2, 7, 4, 2, 9, 6, 5, 4, 6, 6, 6, 7]
 best = [-1 for _ in range(fightDuration)]
-#set total dps to -1 to make debugging easier
-for z in population:
-	z[0] = -1
-
-for z in parents:
-	z[0] = -1
 
 def rankPopulation():
     calculateDps()
 
 def sortRank(child):
-    return child[0] # total dps
+    return -child[0] # rank input array by total dps
 
 def resetPopulation(pop):
     pop[:] = [[0 for _ in range(fightDuration)] for _ in range(numPopulation)]
 
 def assignParents():
     population.sort(key=sortRank)
-    population.reverse()
     parents[:] = population[:len(parents)]
-    #print(parents[0])
-    #print(parents[len(parents)-1])
     if (parents[0][0] > best[0]):
         print('new best ' + str(parents[0][0]))
         best[:] = parents[0]
 
 def generatePopulation(_parents):
     resetPopulation(population)
-    for i in range(0, numPopulation):
+    population[0] = best
+    for i in range(1, numPopulation):
         parent = _parents[int(i/int(numPopulation/numParents))] # distributes weights from parents to children at a rate of numParents:numPopulation
         adrenaline = 100
         cooldowns = [-1 for _ in abilities]
         time = 0
-        abil = 11
+        abil = noneAbilityInd
         for j in range(1, fightDuration):
             if time > 38:
-                #print('from ' + str(j) + ' rest will be 11')
-                population[i][j] = 11
-                #print('  ind#' + str(j) + ' ' + str(population[i]))
+                population[i][j] = noneAbilityInd
                 continue
             staticOrderweight = 6 # increases static order weight of parent's traits by weight/numAbilities
             #if i > 0:
             #   relativeOrderWeight = 5 
             attempts = 35
             for _ in range(0,attempts):
-                abil = random.random() * (numAbilities + staticOrderweight * (1-(parent[j] == 11)))
+                abil = random.random() * (numAbilities + staticOrderweight * (1-(parent[j] == noneAbilityInd)))
                 if abil >= numAbilities:
                     abil = parent[j]
                 else:
@@ -78,8 +70,6 @@ def generatePopulation(_parents):
                 if not haveAdrenFor(abil, adrenaline):
                     continue
                 elif time > cooldowns[abil]:
-                    if abil == 11 and parent[j] == 11:
-                        print('55555555555555555555555555555555555555555----------------------@@@@@@@@@@@@@@@@@@@@@@@33333')
                     adrenaline = min(adrenaline + abilities[abil][2], 100)
                     population[i][j] = abil
                     #print('  abil selected ' + str(abil) + ' ' + str(abilities[abil][0]))
@@ -95,6 +85,8 @@ def haveAdrenFor(abilityInd, adren):
     abilityType = abilities[abilityInd][1]
     if (abilityType == 'basic') or (abilityType == 'threshold' and adren >= 50) or (abilityType == 'ultimate' and adren >= 100):
         return True
+    elif (abilityType == 'special' and adren >= abilities[abilityInd][2]):
+        return True
     return False
 
 
@@ -104,15 +96,11 @@ def calculateDps():
         if i in parents and i[0] <= 0:
             continue
         calculateDamageWithModifiers(i)
-        #children[i][0] = sumDamages()
 
 def sumDamages(damageArray):
     sum = 0
     for num in range(1,fightDuration):
-        #print('base damage of abilities[' + str(num) + '] is ' + str(abilities[damageArray[num]][3]))
-        #print('damage of ind ' + str(num) + ' = ' + str(damageArray[num+1]))
         sum += damageArray[num]
-    #print('sum = ' + str(sum))
     return sum
 
 def calculateDamageWithModifiers(rotation):
@@ -121,24 +109,21 @@ def calculateDamageWithModifiers(rotation):
     unBoundTime = -1 # increase ability-damage of abils preceded by needle strike by 7%
     currentTime = 0
     damageByAbilityIndex = [0 for _ in rotation]
-    for j in range(0, len(rotation)):
-        if j == 0:
-            continue
-        #print('j is ' + str(j))
-        #print('base damage of abilities[' + str(j) + '] is ' + str(abilities[rotation[j]][3]))
+    for j in range(1, len(rotation)):
         baseDamage = damage = abilities[rotation[j]][3]
         if inSwiftness:
             damage *= 1.5
         elif abilities[rotation[j]][0] == "Death's Swiftness":
             inSwiftness=True
-        if abilities[rotation[j]][0] == 'Binding Shot' or 'Tight Bindings':
+        if (abilities[rotation[j]][0] == 'Binding Shot') or (abilities[rotation[j]][0] == 'Tight Bindings'):
             bound = True
+            unBoundTime = currentTime + 9.6
         elif abilities[rotation[j]][0] == 'Piercing Shot':
             if bound and currentTime < unBoundTime:
                 damage *= 1.3
             else:
                 bound = False
-        if abilities[rotation[j]-1][0] == 'Needle Strike':
+        if j > 1 and abilities[rotation[j-1]][int(0)] == 'Needle Strike':
             damage += baseDamage * 0.07
         damageByAbilityIndex[j] = damage
         currentTime += abilities[rotation[j]][5]
